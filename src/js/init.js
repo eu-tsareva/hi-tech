84ed37a535e83ad7a74ad82c1fc5ff64b17d0d90
eu-tsareva <eu.tsareva@gmail.com>
2017-04-28 00:37:16 +0300

    add js for side-nav and list-items, fix width on small screen

--- src/js/init.js
+++ src/js/init.js
@@ -1,7 +1,8 @@
 $(function() {
   var $overlay = $('.modal-overlay'),
       $modalCall = $('.modal-call'),
-      $nav = $('.menu-nav');
+      $nav = $('.menu-nav'),
+      $aside = $('.side-nav');
 
 
   $('.order-call').on('click', function(e) {
@@ -17,8 +18,33 @@ $(function() {
     $overlay.addClass('hide');
   });
 
+  $overlay.on('click', function(e) {
+    e.preventDefault();
+    $('.modal').addClass('hide');
+    $(this).addClass('hide');
+  });
+
   $('.menu-btn').on('click', function(e) {
     e.preventDefault();
     $nav.toggleClass('show');
-  })
+    e.stopPropagation();
+  });
+
+  $(document).on('click', function(e) {
+    $nav.removeClass('show');
+    $aside.removeClass('show');
+  });
+
+  $('.aside-btn').on('click', function(e) {
+    e.preventDefault();
+    $aside.toggleClass('show');
+    e.stopPropagation();
+  });
+
+  $('.steps-list').children('li').map(function(index, item) {
+    var $span = $(item).find('span'),
+        childHeight = $span.height() + $span.position().top,
+        height = (childHeight < $(item).height()) ? $(item).height() : childHeight;
+    $(item).height(height);
+  });
 });
